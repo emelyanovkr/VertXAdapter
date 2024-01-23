b31519a298b5a4d78ff05c126e6c9429061a6e5f
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-01-23 09:28:20 +0300

    importing test frameworks & working with multiple tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,16 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("io.vertx:vertx-stack-depchain:4.5.1"))
-    implementation("io.vertx:vertx-core")
+    implementation("io.vertx:vertx-core:4.5.1")
+    testImplementation("io.vertx:vertx-unit:4.5.1")
+    testImplementation("io.vertx:vertx-junit5:4.5.1")
+
+    // For future testing
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("org.mockito:mockito-core:5.9.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
--- src/main/java/vertx/impl/VertxExecutorRunner.java
+++ src/main/java/vertx/impl/VertxExecutorRunner.java
@@ -2,6 +2,8 @@ package vertx.impl;
 
 import io.vertx.core.Vertx;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 
 public class VertxExecutorRunner
@@ -10,5 +12,19 @@ public class VertxExecutorRunner
     {
         Executor executor = new VertxExecutorImpl(Vertx.vertx());
         executor.execute(() -> System.out.println("Executing Task"));
+
+        List<Runnable> runnableList = new ArrayList<>();
+        for(int i = 0; i < 5; ++i)
+        {
+            int finalI = i;
+            runnableList.add(() -> {
+                System.out.println("Runnable task #" + finalI);
+            });
+        }
+
+        for(Runnable r : runnableList)
+        {
+            executor.execute(r);
+        }
     }
 }
